2ea3299cec29ccb63b32b84faad72e1e468ce1b1
jcomcl <j@jcom.cl>
2025-03-08 12:57:44 +0000

    Rearrange functions in ascending order of specificicity with attention to placing definitions close to (ideally directly above) their consumers

--- gourt.gd
+++ gourt.gd
@@ -7,12 +7,49 @@ enum Direction {UP, DOWN, LEFT, RIGHT}
 var disarray = false #Defines if gourts can be assembled in gourtstack
 var foot_friend: CharacterBody2D
 
-func _ready() -> void:
-	$Gaura.area_entered.connect(gaura_detect)
-
 var rng = RandomNumberGenerator.new()
 func triangular_distribution(lower: float, upper: float) -> float:
 	return rng.randf_range(upper, lower) + rng.randf_range(upper, lower)
+
+func test_spread(f: float, within: float, of: float) -> bool:
+	return (of - within) < f and f < (of + within)
+
+func get_direction(to: Vector2):
+	var angle = rad_to_deg(to.angle()) + 155
+	if test_spread(angle, 30, 65):
+		return Direction.UP
+	if test_spread(angle, 20, 155):
+		return Direction.RIGHT
+	if test_spread(angle, 30, 245) :
+		return Direction.DOWN
+	if test_spread(angle, 20, 340):
+		return Direction.LEFT
+	print("get_direction returned nothing")
+
+func pretty_direction(i:int):
+	if(i == Direction.UP):
+		return "UP"
+	if(i == Direction.DOWN):
+		return "DOWN"
+	if(i == Direction.RIGHT):
+		return "RIGHT"
+	if(i == Direction.LEFT):
+		return "LEFT"
+
+func offset_to(body: Node2D):
+	return body.global_position - global_position
+
+func flip():
+	transform.x *= -1
+
+func gaura_detect(detected_gaura: Node2D):
+	if get_direction(offset_to(detected_gaura)) == Direction.DOWN && disarray == false:
+		var detected_gourt = detected_gaura.get_parent()
+		if !foot_friend: foot_friend = detected_gourt
+		print(gourt_name + " feet friended " + detected_gourt.gourt_name)
+
+func _ready() -> void:
+	$Gaura.area_entered.connect(gaura_detect)
 
 func _physics_process(delta: float) -> void:
 	var target : Vector2
@@ -43,39 +80,3 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.move_toward(target, 20)
 	move_and_slide()
 
-func gaura_detect(detected_gaura: Node2D):
-	if get_direction(offset_to(detected_gaura)) == Direction.DOWN && disarray == false:
-		var detected_gourt = detected_gaura.get_parent()
-		if !foot_friend: foot_friend = detected_gourt
-		print(gourt_name + " feet friended " + detected_gourt.gourt_name)
-
-func pretty_direction(i:int):
-	if(i == Direction.UP):
-		return "UP"
-	if(i == Direction.DOWN):
-		return "DOWN"
-	if(i == Direction.RIGHT):
-		return "RIGHT"
-	if(i == Direction.LEFT):
-		return "LEFT"
-
-func offset_to(body: Node2D):
-	return body.global_position - global_position
-
-func test_spread(f: float, within: float, of: float) -> bool:
-	return (of - within) < f and f < (of + within)
-
-func get_direction(to: Vector2):
-	var angle = rad_to_deg(to.angle()) + 155
-	if test_spread(angle, 30, 65):
-		return Direction.UP
-	if test_spread(angle, 20, 155):
-		return Direction.RIGHT
-	if test_spread(angle, 30, 245) :
-		return Direction.DOWN
-	if test_spread(angle, 20, 340):
-		return Direction.LEFT
-	print("get_direction returned nothing")
-
-func flip():
-	transform.x *= -1

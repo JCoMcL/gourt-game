b4b7ca3195d832151741375bdbd02f9260053973
jcomcl <j@jcom.cl>
2025-03-08 17:33:47 +0000

    implement many changes, incur many bugs

--- gourt.gd
+++ gourt.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-enum Direction {UP, DOWN, LEFT, RIGHT}
+enum Direction {UP, DOWN, LEFT, RIGHT, NONE}
 @export var facing = Direction.LEFT
 @export var gourt_name = "gourt" #For debug purposes, serves no gameplay value. TODO revise 
 @export var foot_friend: CharacterBody2D
 @export var disarray = false #Defines if gourts can be assembled in gourtstack
+
+@export var debug_mode = false
+func debug_print(s):
+	if debug_mode: print(s)
 
 var rng = RandomNumberGenerator.new()
 func triangular_distribution(lower: float, upper: float) -> float:
@@ -28,11 +32,30 @@ func get_direction(to: Vector2):
 func pretty_direction(i:int):
 	return Direction.keys()[i]
 
+func x_direction(x: float):
+	if absf(x) < 10: return Direction.NONE
+	return Direction.RIGHT if x > 0 else Direction.LEFT
+func y_direction(y: float):
+	if absf(y) < 10: return Direction.NONE
+	return Direction.UP if y > 0 else Direction.DOWN #WARN: untested
+func vec_direction(v: Vector2):
+	if absf(v.x) > absf(v.y):
+		return x_direction(v.x)
+	return y_direction(v.y)
+
+func flip() -> void:
+	transform.x *= -1
+
+func set_facing(d: Direction) -> void:
+	if d == Direction.NONE:
+		return
+	assert(d == Direction.LEFT || d == Direction.RIGHT)
+	if d != facing:
+		flip()
+		facing = d
+
 func offset_to(body: Node2D):
 	return body.global_position - global_position
-
-func flip():
-	transform.x *= -1
 
 func stack_onto(o: Node2D):
 	foot_friend = o
@@ -45,37 +68,44 @@ func break_stack(impulse_scale: int = 1) -> void:
 	disarray = true
 	foot_friend = null
 	velocity += Vector2(
-		triangular_distribution(-200.0, 200.0),
-		triangular_distribution(-100, -200)
+		triangular_distribution(-2, 2),
+		triangular_distribution(-1, -2)
 	) * impulse_scale
 
-func _ready() -> void:
-	$Gaura.area_entered.connect(gaura_detect)
+var walk_target: float
+func snap_to(target: Vector2, snappiness: float = 100):
+	velocity = velocity.move_toward(to_local(target), snappiness)
+
+func get_move_input() -> float:
+	if foot_friend:
+		return 0
+	return Input.get_axis("go left","go right")
 
 func _input(ev: InputEvent) -> void:
 	if ev.is_action_pressed("break stack") && foot_friend:
-		break_stack(2)
+		break_stack(200)
+
+func _process(delta: float) -> void:
+	set_facing(x_direction( velocity.x if foot_friend else walk_target ))
+	if walk_target != 0:
+		$Body.play("transportative")
+	else:
+		$Body.play("idleative")
 
 func _physics_process(delta: float) -> void:
-	var target: Vector2
-	if foot_friend:
-		$Body.play("idleative")
-		target = offset_to(foot_friend) + Vector2.UP * 70
-	else:
-		target.x = Input.get_axis("go left","go right") * 200
-		# Add the gravity.
-		if not is_on_floor():
-			target += get_gravity() * delta * 100
+	walk_target = get_move_input() * 200
 
-		if target.x != 0:
-			$Body.play("transportative")
-			var t_facing = Direction.RIGHT if target.x > 0 else Direction.LEFT
-			if t_facing != facing:
-					flip()
-					facing = t_facing
-					print(pretty_direction(facing))
-		else:
-			$Body.play("idleative")
-			
-	velocity = velocity.move_toward(target, 20)
+	if !is_on_floor() && !foot_friend:
+		velocity += get_gravity() * delta
+
+	if foot_friend:
+		snap_to(foot_friend.position + Vector2.UP * 70) #TODO add snap-point to gourt.tscn instead of guessing
+		walk_target = 0
+
+	if walk_target != 0:
+		velocity.x = move_toward(velocity.x, walk_target, 20)
+
 	move_and_slide()
+
+func _ready() -> void:
+	$Gaura.area_entered.connect(gaura_detect)

80686598371413b5df548d6ff576e3040273756b
jcomcl <j@jcom.cl>
2025-06-20 17:00:00 +0100

    bruh
    Add a crude speech bubble which visually displays the conversation complete with proper positioning

---
+++ tools/speech_bubble.gd
@@ -0,0 +1,23 @@
+@tool
+extends ColorRect
+class_name SpeechBubble
+
+@export var text: String = "Hello, World!":
+	set(value):
+		text = value
+		if label:
+			label.text = value
+			update_size()
+
+@onready var label = $Label
+
+func _ready():
+	text = text
+	
+func update_size():
+	var font = label.get_theme_font("font")
+	if font:
+		var text_line = TextLine.new()
+		text_line.add_string(label.text, font, label.get_theme_font_size("normal_font_size"))
+		var text_size = text_line.get_size()
+		size = text_size

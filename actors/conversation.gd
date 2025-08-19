795c94a40323c610c69fd6967ad44e91a2de77b4
jcomcl <j@jcom.cl>
2025-08-19 18:31:29 +0100

    add the ability to call code actor methods from the conversation

--- actors/conversation.gd
+++ actors/conversation.gd
@@ -8,7 +8,7 @@ class Line:
 	func _init(text: String, speaker: Node2D):
 		self.text = text
 		self.speaker = speaker
-		
+
 @onready var lines = [ Line.new("Look at that.", actors[0]),
 	Line.new("This a seems like a violation of workplace safety code.", actors[0]),
 	Line.new("29 CFR 1910.212", actors[0]),
@@ -18,27 +18,35 @@ class Line:
 	Line.new("We'll need to bring this finding to the owner the building at once.", actors[0]),
 	Line.new("The Boss. His office is that way.", actors[1]),
 	Line.new("Thank you for your cooperation", actors[0]),
+	actors[1].die
 ]
 
 var counter = 0
 var speech_bubble: SpeechBubble
 func display_line():
-	var line: Line = lines[counter]
-	print("%s: %s" % [line.speaker.name, line.text])
+	if counter >= lines.size():
+		if speech_bubble:
+			speech_bubble.queue_free()
+		return
 
-	if ! speech_bubble:
-		speech_bubble = SpeechTherapist.say(line.speaker, line.text)
-		speech_bubble.auto_expire = false
-		speech_bubble.on_done_showing.connect(display_line)
-	else:
-		speech_bubble.reset_text(line.text)
-		speech_bubble.speaker = line.speaker
-	speech_bubble.anneal_position(10)
-
+	var line = lines[counter]
 	counter += 1
-	if counter == lines.size():
-		speech_bubble.on_done_showing.disconnect(display_line)
-		speech_bubble.auto_expire = true
+
+	if line is Line: # Yeah, I know this is confusing
+		print("%s: %s" % [line.speaker.name, line.text])
+
+		if ! speech_bubble:
+			speech_bubble = SpeechTherapist.say(line.speaker, line.text)
+			speech_bubble.auto_expire = false
+			speech_bubble.on_done_showing.connect(display_line)
+		else:
+			speech_bubble.reset_text(line.text)
+			speech_bubble.speaker = line.speaker
+		speech_bubble.anneal_position(10)
+	elif line is Callable:
+		await line.call()
+		display_line()
+
 
 func activate():
 	if lines.size() > 0:

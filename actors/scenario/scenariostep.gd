9b6a59f0dd57e4bd64cf50d9cda5f28c561f0bf5
jcomcl <j@jcom.cl>
2025-08-20 11:27:28 +0100

    delete scenario because red good, green bad

--- actors/scenario/scenariostep.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name ScenarioStep
-
-enum ActionType { COMMAND, ACTIVATE, COMMENTARY, WAIT }
-
-@export var action: ActionType = ActionType.COMMAND
-@export var target: NodePath
-@export var params: Dictionary = {}

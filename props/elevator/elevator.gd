01b2b841afb3b133e856e02c9f08d048d204d2fc
jcomcl <j@jcom.cl>
2025-08-24 10:21:45 +0100

    make elvator only a door while open

--- props/elevator/elevator.gd
+++ props/elevator/elevator.gd
@@ -4,14 +4,23 @@ var open_state = false
 func open():
 	if not open_state:
 		open_state = true
-		monitorable = true
+		update_state()
 		$AnimationPlayer.play("opening")
 
 func close():
 	if open_state:
 		open_state = false
-		monitorable = false
 		$AnimationPlayer.play("closing")
+
+func update_state():
+	collision_layer = Clision.layers["door"] * int(open_state)
+
+func on_animation_finished(anim_name: StringName):
+	update_state()
+
+func _ready():
+	update_state()
+	$AnimationPlayer.animation_finished.connect(on_animation_finished)
 
 func interact(operator: Node) -> bool:
 	operator.reparent($Frame/Interior/Background)
